5477ddd2b6fc44b073f282d2c0051a151d696f8a
Oleh <mihajlovaoleg@gmail.com>
2025-09-05 18:01:03 +0200

    test logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,3 +34,10 @@ import { LivelinessModule } from './business-layer/liveliness/liveliness.module'
   providers: [],
 })
 export class AppModule {}
+
+
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PORT);
+console.log(process.env.DB_USERNAME);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_NAME);
