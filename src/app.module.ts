396a9b152d9867c3cfc7be48e624c06589b85010
Oleh <mihajlovaoleg@gmail.com>
2025-09-05 15:51:39 +0200

    changed port logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { LivelinessModule } from './business-layer/liveliness/liveliness.module'
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3000,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD || '',
       database: process.env.DB_NAME,
